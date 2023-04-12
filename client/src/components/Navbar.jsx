4a0e9dc9f0eaf573df10ad46245f9bb93e78a102
FinoanaRandria <fitiavanarex4@gmail.com>
2023-04-12 21:00:38 +0300

    debut interface

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import {GiExitDoor} from 'react-icons/gi'
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar bg-[#7005e7] text-primary-content flex'>
 
-<div className="navbar bg-[#7005e7] text-primary-content">
+<div className=" containerWrap justify-between ">
   <a className="btn btn-ghost normal-case text-xl">Echat 2.0</a>
+   <button className='flex gap-2 '>Logout <span className='mt-1'><GiExitDoor/></span></button>
 </div>
     </div>
   )
